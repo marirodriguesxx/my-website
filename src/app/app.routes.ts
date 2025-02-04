fb5c3fe78b42c3f74eed38fbd26d35694f183573
marirodriguesxx <marianarodriguesdesantana@gmail.com>
2025-02-03 23:58:38 -0300

    Defining landing page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/landing', pathMatch: 'full' },  // Add default redirection
+    { path: 'landing', component: LandingComponent }
+];
---
+++ src/app/components/landing/landing.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent implements OnInit {
+  dynamicText: string = "I’m Mariana Rodrigues, a computer scientist exploring the underworlds of technology (and loving it). Come explore my projects and venture with me!";
+  
+  ngOnInit() {
+    this.typeText();
+  }
+
+  typeText() {
+    let index = 0;
+    const textElement = document.getElementById('dynamic-text');
+    if (textElement) {
+      const interval = setInterval(() => {
+        textElement.innerHTML += this.dynamicText.charAt(index);
+        index++;
+        if (index === this.dynamicText.length) {
+          clearInterval(interval);
+        }
+      }, 80); // Adjust speed by changing the interval time
+    }
+  }
+}
